04f7439aae11e1905dc306fe39dfbff0d914042c
Josias Daniel de Souza Moreira <josiasdanielmoreirasouza@gmail.com>
2025-07-15 11:28:52 -0300

    .

--- projetoteste.py
+++ projetoteste.py
@@ -1,8 +1,22 @@
 pes = {}
+v = 0
+while True:
+    pes['gmail'] = str(input('gmail: '))
+    if '@' in pes['gmail']:
+        v = 1
+    else:
+        v = 0
+    pes['idade'] = int(input('Idade: '))
+    if 0 < pes['idade']:
+        v = 1
+    else:
+        v = 0
+    pes['Nome'] = str(input('Nome: '))
 
-pes['Nome'] = str(input('Nome: '))
-pes['gmail'] = str(input('gmail: '))
-pes['idade'] = int(input('Idade: '))
+    if v == 1:
+        break
+    else:
+        print('Faz de novo mula')
 print(pes)
 for c, v in pes:
     print(c)
